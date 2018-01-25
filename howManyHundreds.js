0569311df5b7d09e3c82ee32f925903bbabc73d7
Craig <craigricetong@gmail.com>
2018-01-25 19:26:13 +0000

    Adds conditionals exercises

---
+++ howManyHundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(num) {
+  return Math.floor (num / 100);
+}
+
+
+console.log(howManyHundreds(1234));
+console.log(howManyHundreds(55));
+console.log(howManyHundreds(100));
+console.log(howManyHundreds(1234));
+console.log(howManyHundreds(200));
+console.log(howManyHundreds(123456));
